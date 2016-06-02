5a255995d9e756f30c72df96a13e98546c26beef
Alex Chan <fundation@nonstopcoder.com>
2016-06-01 21:57:53 -0700

    Waiting for Mongo to finish loading before starting fundation

--- index.js
+++ index.js
@@ -33,23 +33,29 @@ Fundation.prototype.init = function (options) {
     require('./lib/config.js')(parent, options);
 
     // Database / Storage
-    require('./lib/mysql.js')(parent);
-    require('./lib/mongodb.js')(parent);
-    require('./lib/memcached.js')(parent);
+    var promises = [];
+    promises.push(require('./lib/mysql.js')(parent));
+    promises.push(require('./lib/mongodb.js')(parent));
+    promises.push(require('./lib/memcached.js')(parent));
 
-    // Middleware
-    require('./middleware/logging.js')(parent);
-    require('./middleware/statics.js')(parent);
-    require('./middleware/basic-auth.js')(parent);
-    require('./middleware/models.js')(parent, self);
-    require('./middleware/middleware.js')(parent);
-    require('./middleware/views.js')(parent);
-    require('./middleware/controllers.js')(parent);
+    // Once the DB connections are connected
+    // Then we can setup all of the middleware
+    Promise.all(promises)
+    .then(function(){
+      // Middleware
+      require('./middleware/logging.js')(parent);
+      require('./middleware/statics.js')(parent);
+      require('./middleware/basic-auth.js')(parent);
+      require('./middleware/models.js')(parent, self);
+      require('./middleware/middleware.js')(parent);
+      require('./middleware/views.js')(parent);
+      require('./middleware/controllers.js')(parent);
+    });
 
   });
 
   return app;
 
-}
+};
 
 module.exports = new Fundation();
