6f7c7d39243f706ee2aeb70d32572509f881e085
Alex Chan <alex.chan@demandmedia.com>
2017-04-04 14:03:47 -0700

    Adding plugin support

--- src/app.js
+++ src/app.js
@@ -4,14 +4,10 @@ import store from '../../../store'
 import router from '../../../router'
 import meta from 'vue-meta'
 import { sync } from 'vuex-router-sync'
+import * as plugins from '../../../plugins'
 import * as filters from '../../../filters'
 import axios from 'axios'
 import cookie from 'vue-cookie'
-
-// http://element.eleme.io/#/en-US/component/i18n
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en'
-Vue.use(ElementUI, { locale })
 
 // https://medium.com/the-vue-point/retiring-vue-resource-871a82880af4#.w5c4snp5p
 // access this.$http like in vue-resource
@@ -26,6 +22,16 @@ Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key])
 })
 
+// Register plugins
+Object.keys(plugins).forEach(key => {
+  plugins[key](Vue)
+})
+
+// https://github.com/declandewet/vue-meta
+Vue.use(meta)
+
+Vue.use(cookie)
+
 // create the app instance.
 // here we inject the router and store to all child components,
 // making them available everywhere as `this.$router` and `this.$store`.
@@ -34,11 +40,6 @@ const app = new Vue({
   store,
   render: h => h(App)
 })
-
-// https://github.com/declandewet/vue-meta
-Vue.use(meta)
-
-Vue.use(cookie)
 
 // expose the app, the router and the store.
 // note we are not mounting the app here, since bootstrapping will be
