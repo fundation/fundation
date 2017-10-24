5da47bc5838653fbd649756c950e7cfdf76bb30d
Michael Ball <michael.ball@demandmedia.com>
2017-10-24 14:10:49 -0700

    Update Redis client set logic and update health check URL

--- middleware/health.js
+++ middleware/health.js
@@ -6,24 +6,10 @@ module.exports = function(app) {
 
   debug("Setting up Health Check");
 
-  app.get('/fun_test', function (req, res) {
-    var config = app.get('config');
-
-    var status = 200;
-    var redis = false;
-
-    if (config.redis) {
-      redis = app.get('redis').ready;
-      if ( !redis ) {
-        status = 500;
-      }
-    }
-
-    res.status(status).json({
-      env: app.get('env'),
-      redis: redis
+  app.get('/health-check', function (req, res) {
+    res.status(200).json({
+      ok: 1
     });
-
   });
 
 };
