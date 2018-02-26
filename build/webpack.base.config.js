332d22092402632b404384593ce4b2b9345e1c82
John Ko <john.ko@leafgroup.com>
2018-02-26 11:02:38 -0800

    Modify webpack builds to include unique hash in js assets

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -13,7 +13,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, '../../../dist'),
     publicPath: '/dist/',
-    filename: '[name].js?v=[hash]'
+    filename: '[name]_[hash].js'
   },
   resolve: { // no clue what this actually does yet
     alias: {
