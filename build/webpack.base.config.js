572ff58178b1d0229b7840ae1162961a993f3583
John Ko <john.ko@leafgroup.com>
2018-02-27 15:56:34 -0800

    Add s3 uploads to fundation

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,23 +1,56 @@
+const _ = require('lodash')
 const path = require('path')
 const vueConfig = require('./vue-loader.config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const webpack = require('webpack')
+const configMerger = require('../lib/configMerger')
+const S3Plugin = require('webpack-s3-plugin')
 
 const isProdOrStage = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'
+const rootPath = path.resolve(__dirname, '../../../')
+const config = configMerger(process.env.NODE_ENV, rootPath + '/')
+
+const plugins = isProdOrStage ? [
+  new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false }
+  }),
+  new webpack.optimize.ModuleConcatenationPlugin(),
+  new ExtractTextPlugin({
+    filename: 'common_[chunkhash].css'
+  }),
+  new S3Plugin({
+    include: /.*\.(css|js)/,
+    // s3Options are required
+    s3Options: {
+      accessKeyId: process.env.S3_ACCESS,
+      secretAccessKey: process.env.S3_SECRET,
+      region: 'us-east-1'
+    },
+    s3UploadOptions: {
+      Bucket: _.get(config, 's3.bucket', ''),
+    },
+    basePath: 'assets'
+    // cdnizerOptions: {
+    //   defaultCDNBase: 'http://asdf.ca'
+    // }
+  })
+] : [
+  new FriendlyErrorsPlugin()
+]
 
 module.exports = {
   devtool: isProdOrStage
     ? false
     : '#cheap-module-eval-source-map',
   output: {
-    path: path.resolve(__dirname, '../../../dist'),
+    path: path.resolve(rootPath + '/dist'),
     publicPath: '/dist/',
     filename: '[name]_[hash].js'
   },
   resolve: { // no clue what this actually does yet
     alias: {
-      'public': path.resolve(__dirname, '../../../public')
+      'public': path.resolve(rootPath + '/public')
     }
   },
   module: {
@@ -68,17 +101,5 @@ module.exports = {
       },
     ]
   },
-  plugins: isProdOrStage
-    ? [
-        new webpack.optimize.UglifyJsPlugin({
-          compress: { warnings: false }
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new ExtractTextPlugin({
-          filename: 'common.css?v=[chunkhash]'
-        })
-      ]
-    : [
-        new FriendlyErrorsPlugin()
-      ]
+  plugins: plugins
 }
